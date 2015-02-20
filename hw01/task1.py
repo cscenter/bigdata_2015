868eeec5be5a145e7566abfff80cda0af690ca26
Ravil <dehasi@yandex.ru>
2015-02-20 23:52:22 +0300

    написал свою версию функции demo() в общем во тмой первый коммит

--- hw01/task1.py
+++ hw01/task1.py
@@ -24,6 +24,24 @@ def demo():
       # удаляем символ перевода строки 
       print(line[:-1])
 
+# дял начала я сделаю свою демо которя будет показывать имеющиеся 
+# DFS файлы, разположение их фрагментов и содержимое заданного фрагмента 
+# с заданного  сервера
+
+def demo2(server = "cs0", partitions = "partitions"):
+  for f in dfs.files():
+    print("File {0} consists of fragments {1}".format(f.name, f.chunks))
+
+  for c in dfs.chunk_locations():
+    print("Chunk {0} sits on chunk server {1}".format(c.id, c.chunkserver))
+
+  print("\nThe contents of chunk partitions:")
+  with dfs.get_chunk_data(server, partitions) as f:
+    for line in f:
+      # удаляем символ перевода строки 
+      print(line[:-1])
+
+
 # Эту функцию надо реализовать. Функция принимает имя файла и 
 # возвращает итератор по его строкам. 
 # Если вы не знаете ничего про итераторы или об их особенностях в Питоне,
@@ -34,7 +52,10 @@ def get_file_content(filename):
 
 # эту функцию надо реализовать. Она принимает название файла с ключами и возвращает 
 # число
-def calculate_sum(keys_filename):
-  raise "Comment out this line and write your code below"
+def calculate_sum(keys_filename = "./data/keys"):
+  f = open(keys_filename, "r")
+  print(f.readline())
+#  raise "Comment out this line and write your code below"
 
-demo()
+#demo2()
+calculate_sum()
