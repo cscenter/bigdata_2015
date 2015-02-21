05a8cf144a23c64bb03637b27ff8ce92337864d3
neznaika94 <andreyspbsu94@gmail.com>
2015-02-21 22:50:39 +0000

    version 1.2

--- hw01/task1.py
+++ hw01/task1.py
@@ -43,7 +43,9 @@ def get_chunks(shard_name):
           
 def get_server_for_chunk(chunk):
   for c in dfs.chunk_locations():
-      print(1)              
+      if c.id == chunk:
+          return c.chunkserver
+       
 
 # эту функцию надо реализовать. Она принимает название файла с ключами и возвращает
 # число
@@ -53,16 +55,29 @@ def calculate_sum(keys_filename):
   
   # При использовании http_dfs читаем с данного сервера
   #chunk_iterator = dfs.get_chunk_data("104.155.8.206", "partitions")
-  for key in file:
+  for keys in file:
+      key = keys[:-1]
       for line in dfs.get_chunk_data("cs0", "partitions"):
           (begin, end, shard_name) = line[:-1].split(' ')
           if key >= begin and key <= end:
               chunks = get_chunks(shard_name)
               for chunk in chunks:
+                  haveFoundKey = False #уже нашли текущий ключ
                   server_name = get_server_for_chunk(chunk)
+                  for line in dfs.get_chunk_data(server_name, chunk):
+                      if line == '\n':
+                          break
+                      (chunk_key, chunk_value) = line[:-1].split(' ')
+                      if key == chunk_key:
+                          result += int(chunk_value)
+                          haveFoundKey = True
+                          break
+                  if haveFoundKey:
+                      break
               break
    
   return result           
   
-demo()
-#calculate_sum('data/keys')
+#demo()
+result_sum = calculate_sum('data/keys')
+print(result_sum)
