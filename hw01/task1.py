3ff98a6014c89e446138c5efa8ad953e8c6e3c7a
neznaika94 <andreyspbsu94@gmail.com>
2015-02-21 23:20:36 +0000

    version 1.3

--- hw01/task1.py
+++ hw01/task1.py
@@ -5,32 +5,8 @@ import test_dfs as dfs
 
 #import http_dfs as dfs
 
-# Демо показывает имеющиеся в DFS файлы, расположение их фрагментов
-# и содержимое фрагмента "partitions" с сервера "cs0"
-# (не рассчитывайте, что эти две константы останутся неизменными в http_dfs. Они
-#  использованы исключительно для демонстрации)
-def demo():
- # for f in dfs.files():
-  #  print("File {0} consists of fragments {1}".format(f.name, f.chunks))
-  #  print('             1111')
-
-  for c in dfs.chunk_locations():
-    print("Chunk {0} sits on chunk server {1}".format(c.id, c.chunkserver))
-
-  # Дальнейший код всего лишь тестирует получение фрагмента, предполагая, что известно,
-  # где он лежит. Не рассчитывайте, что этот фрагмент всегда будет находиться
-  # на использованных тут файл-серверах
-
-  # При использовании test_dfs читаем из каталога cs0
- # chunk_iterator = dfs.get_chunk_data("cs0", "partitions")
- 
- 
-
 # Эту функцию надо реализовать. Функция принимает имя файла и
 # возвращает итератор по его строкам.
-# Если вы не знаете ничего про итераторы или об их особенностях в Питоне,
-# погуглите "python итератор генератор". Вот например
-# http://0agr.ru/blog/2011/05/05/advanced-python-iteratory-i-generatory/
 def get_file_content(filename):
   file = open(filename)
   for line in file:
@@ -46,16 +22,13 @@ def get_server_for_chunk(chunk):
       if c.id == chunk:
           return c.chunkserver
        
-
 # эту функцию надо реализовать. Она принимает название файла с ключами и возвращает
 # число
 def calculate_sum(keys_filename):
   result = 0  
-  file = open(keys_filename)
-  
   # При использовании http_dfs читаем с данного сервера
   #chunk_iterator = dfs.get_chunk_data("104.155.8.206", "partitions")
-  for keys in file:
+  for keys in get_file_content(keys_filename):
       key = keys[:-1]
       for line in dfs.get_chunk_data("cs0", "partitions"):
           (begin, end, shard_name) = line[:-1].split(' ')
