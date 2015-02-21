582dce912805eefab9a1587147b28d0c8b37d44f
Svyatoslav Gryaznov <nightuser@ya.ru>
2015-02-21 10:24:30 +0300

    Reformat files according to PEP8; add .gitignore

--- hw01/task1.py
+++ hw01/task1.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 # encoding: utf8
 
 # Для быстрого локального тестирования используйте модуль test_dfs
@@ -7,34 +7,37 @@ import test_dfs as dfs
 # Для настоящего тестирования используйте модуль http_dfs
 # import http_dfs as dfs
 
+
 # Демо показывает имеющиеся в DFS файлы, расположение их фрагментов
 # и содержимое фрагмента "partitions" с сервера "cs0"
-# (не рассчитывайте, что эти две константы останутся неизменными в http_dfs. Они
-#  использованы исключительно для демонстрации)
+# (не рассчитывайте, что эти две константы останутся неизменными в http_dfs.
+# Они использованы исключительно для демонстрации)
 def demo():
-  for f in dfs.files():
-    print("File {0} consists of fragments {1}".format(f.name, f.chunks))
+    for f in dfs.files():
+        print("File {0} consists of fragments {1}".format(f.name, f.chunks))
 
-  for c in dfs.chunk_locations():
-    print("Chunk {0} sits on chunk server {1}".format(c.id, c.chunkserver))
+    for c in dfs.chunk_locations():
+        print("Chunk {0} sits on chunk server {1}".format(c.id, c.chunkserver))
 
-  print("\nThe contents of chunk partitions:")
-  with dfs.get_chunk_data("cs0", "partitions") as f:
-    for line in f:
-      # удаляем символ перевода строки 
-      print(line[:-1])
+    print("\nThe contents of chunk partitions:")
+    with dfs.get_chunk_data("cs0", "partitions") as f:
+        for line in f:
+            # удаляем символ перевода строки
+            print(line[:-1])
 
-# Эту функцию надо реализовать. Функция принимает имя файла и 
-# возвращает итератор по его строкам. 
+
+# Эту функцию надо реализовать. Функция принимает имя файла и
+# возвращает итератор по его строкам.
 # Если вы не знаете ничего про итераторы или об их особенностях в Питоне,
 # погуглите "python итератор генератор". Вот например
 # http://0agr.ru/blog/2011/05/05/advanced-python-iteratory-i-generatory/
 def get_file_content(filename):
-  raise "Comment out this line and write your code below"
+    raise "Comment out this line and write your code below"
 
-# эту функцию надо реализовать. Она принимает название файла с ключами и возвращает 
-# число
+
+# эту функцию надо реализовать. Она принимает название файла с ключами и
+# возвращает число
 def calculate_sum(keys_filename):
-  raise "Comment out this line and write your code below"
+    raise "Comment out this line and write your code below"
 
 demo()
--- hw01/test_dfs.py
+++ hw01/test_dfs.py
@@ -3,33 +3,35 @@
 from collections import namedtuple
 import json
 
+
 def _json_object_hook(d): return namedtuple('X', d.keys())(*d.values())
+
+
 def json2obj(data): return json.loads(data, object_hook=_json_object_hook)
+
 
 # Получает от "мастера" список файлов и входящих в каждый файл фрагментов
 # Возвращает список объектов с полями "name": String и "chunks": String[]
-# "name" - это имя файла, "chunks" - список строковых идентификаторов 
+# "name" - это имя файла, "chunks" - список строковых идентификаторов
 # фрагментов в том порядке, в котором они следуют в файле
 def files():
-  with open("data/files") as f:
-    return json2obj(f.read())
+    with open("data/files") as f:
+        return json2obj(f.read())
+
 
 # Получает от "мастера" расположение фрагмента на файловых серверах.
 # Так как репликация для нашей задачи несущественна, то файловый сервер
 # у каждого фрагмента один.
 # Возвращает список объектов с полями "id": String и "chunkserver": String
-# где "id" - идентификатор фрагмента, "chunkserver" - идентификатор файлового сервера,
+# где "id" - идентификатор фрагмента,
+# "chunkserver" - идентификатор файлового сервера,
 # на котором фрагмент хранится
 def chunk_locations():
-  with open("data/chunk_locations") as f:
-    return json2obj(f.read())
+    with open("data/chunk_locations") as f:
+        return json2obj(f.read())
+
 
 # Возвращает содержимое указанного фрагмента с указанного файлового сервера
 # в виде потока
 def get_chunk_data(chunk_server_id, chunk_id):
-  return open("data/%s/%s" % (chunk_server_id, chunk_id))
-
-
-
-
-
+    return open("data/%s/%s" % (chunk_server_id, chunk_id))
