f8e29e0cd85d919d43eab641bdb4e8624ed14120
LenaVolzhina <LenaVolzhina@users.noreply.github.com>
2015-02-21 10:48:09 +0300

    Update task1.py

--- hw01/task1.py
+++ hw01/task1.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python
 # encoding: utf8
 
+import time
+
 # Для быстрого локального тестирования используйте модуль test_dfs
-import test_dfs as dfs
+#import test_dfs as dfs
 
 # Для настоящего тестирования используйте модуль http_dfs
-#import http_dfs as dfs
+import http_dfs as dfs
 
 # Демо показывает имеющиеся в DFS файлы, расположение их фрагментов
 # и содержимое фрагмента "partitions" с сервера "cs0"
@@ -18,19 +20,11 @@ def demo():
   for c in dfs.chunk_locations():
     print("Chunk {0} sits on chunk server {1}".format(c.id, c.chunkserver))
 
-  # Дальнейший код всего лишь тестирует получение фрагмента, предполагая, что известно,
-  # где он лежит. Не рассчитывайте, что этот фрагмент всегда будет находиться
-  # на использованных тут файл-серверах
-
-  # При использовании test_dfs читаем из каталога cs0
-  chunk_iterator = dfs.get_chunk_data("cs0", "partitions")
-
-  # При использовании http_dfs читаем с данного сервера
-  #chunk_iterator = dfs.get_chunk_data("104.155.8.206", "partitions")
   print("\nThe contents of chunk partitions:")
-  for line in chunk_iterator:
-    # удаляем символ перевода строки
-    print(line[:-1])
+  with dfs.get_chunk_data("cs0", "partitions") as f:
+    for line in f:
+      # удаляем символ перевода строки
+      print(line[:-1])
 
 # Эту функцию надо реализовать. Функция принимает имя файла и
 # возвращает итератор по его строкам.
@@ -38,11 +32,69 @@ def demo():
 # погуглите "python итератор генератор". Вот например
 # http://0agr.ru/blog/2011/05/05/advanced-python-iteratory-i-generatory/
 def get_file_content(filename):
-  raise "Comment out this line and write your code below"
+    file = open(filename)
+    for line in file:
+        yield line
+
+def get_shard_by_key(key, partitions):
+    for line in partitions:
+        temp = line[:-1].split(' ')
+        if len(temp) != 3:
+            raise Exception("Invalid input format")
+        L, R, shard = temp
+        if (L <= key) & (key <= R):
+            return shard
+    raise Exception("Can't find shard for key {0}!".format(key))
+
+def get_chunks_by_shard(shard):
+    for f in dfs.files():
+        if f.name == shard:
+            return f.chunks
+    raise Exception("Where is shard {0}?".format(shard))
+
+def get_chunk_server(chunk):
+    for c in dfs.chunk_locations():
+        if chunk == c.id:
+            return c.chunkserver
+    raise Exception("Where is chunk {0}?".format(chunk))
 
 # эту функцию надо реализовать. Она принимает название файла с ключами и возвращает
 # число
 def calculate_sum(keys_filename):
-  raise "Comment out this line and write your code below"
+    start_time = time.clock()
 
-demo()
+    partition_chunk = 0
+    for f in dfs.files():
+        if f.name == "/partitions":
+            partition_chunk = f.chunks[0]
+            break
+    if partition_chunk == 0:
+        raise Exception("There is no file with partitions")
+
+    partition_cs = 0
+    for c in dfs.chunk_locations():
+        if c.id == partition_chunk:
+            partition_cs = c.chunkserver
+    if partition_cs == 0:
+        raise Exception("There is no file with partitions")
+
+    # узнали, где находится файл, описывающий разбиение данных
+
+    #print("Time used for finding partitions time: {0} seconds".format((time.clock() - start_time)))
+
+    result_sum = 0
+    for key in get_file_content(keys_filename):
+        key = key[:-1]
+        shard = get_shard_by_key(key, dfs.get_chunk_data(partition_cs, partition_chunk))
+        chunks = get_chunks_by_shard(shard)
+        for ch in chunks:
+            with dfs.get_chunk_data(get_chunk_server(ch), ch) as f:
+                for line in f:
+                    if (len(line) > 0) & (line.split(' ')[0] == key):
+                        result_sum += int(line.split(' ')[1])
+                        break
+    #print("Used time: {0} seconds".format(int(time.clock() - start_time)))
+    return result_sum
+
+#demo()
+print(calculate_sum("data\keys"))
