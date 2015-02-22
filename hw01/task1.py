0e10425e3734b2695630329be728c56045fe4b51
Mark <gellmark@gmail.com>
2015-02-22 04:29:00 +0300

    First version of the first homework

--- hw01/task1.py
+++ hw01/task1.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-# encoding: utf8
+#encoding: utf-8
 
 # Для быстрого локального тестирования используйте модуль test_dfs
 import test_dfs as dfs
@@ -38,11 +38,100 @@ def demo():
 # погуглите "python итератор генератор". Вот например
 # http://0agr.ru/blog/2011/05/05/advanced-python-iteratory-i-generatory/
 def get_file_content(filename):
-  raise "Comment out this line and write your code below"
+  with open(filename) as inp:
+    for line in inp:
+      if line.strip():
+        yield line.strip()
 
+
+# Возвращает chunks данного файла
+def chunks_by_name(name):
+  for file in dfs.files():
+    try:
+      if file.name == name:
+        return file.chunks
+    except:
+      raise Exception("Can not retrieve file name: {}".format(file))
+  raise Exception("No such file {}".format(name))
+
+#Получает сервер на котором хранится chunk
+def server_by_chunk_id(id):
+  for chunk in dfs.chunk_locations():
+    try:
+      if chunk.id == id:
+        return chunk.chunkserver
+    except:
+      raise Exception("Can not chunk id: {}".format(chunk))
+  raise Exception("No chunk found with id:{}".format(id))
+
+
+# Получает шард в котором хранится диапазон ключей соответсвующий ключу
+def get_shard_by_key(key, partition_server, partition_chunk):
+  for line in dfs.get_chunk_data(partition_server, partition_chunk):
+      if line.strip():
+        if len(line.strip().split()) == 3:
+          lkey, rkey, shard = line.strip().split()
+          if lkey <= key <= rkey:
+            return shard
+        else:
+          raise Exception("Invalid partition file format:{}".format(line))
+  return None
+
+
+# Получает значение соответсвующее ключу
+def get_value(key, chunks):
+  for chunk in chunks:
+    server = server_by_chunk_id(chunk)
+    for line in dfs.get_chunk_data(server, chunk):
+      if line.strip() and len(line.strip().split()) == 2:
+        p_key, value = line.strip().split()
+        if p_key == key:
+          if value.isdigit():
+            return int(value)
+          else:
+            raise Exception("Invalid value:{}".format(value))
+  raise Exception("Can not find key:{}".format(key))
+
+
+#на удаленном сервере в файле partitions имена шардов не начинаются с '/'
+def modify_shard(shard):
+  if shard[0] == '/':
+    return shard[1:]
+  else:
+    return shard
 # эту функцию надо реализовать. Она принимает название файла с ключами и возвращает
 # число
-def calculate_sum(keys_filename):
-  raise "Comment out this line and write your code below"
+def calculate_sum(keys_filename=None, use_remote_keys=False):
+  partition_chunks = chunks_by_name('/partitions')
+  ans = 0
+  #Файл /partitions может быть разбин на несколько chunk
+  if not use_remote_keys:
+    keys = get_file_content(keys_filename)
+  else:
+    keys_chunk = chunks_by_name('/keys')[0]
+    key_server = server_by_chunk_id(keys_chunk)
+    keys = dfs.get_chunk_data(key_server, keys_chunk)
+  for key_n in keys:
+    key = key_n.strip()
+    found = False
+    for partition_chunk in partition_chunks:
+      partition_server = server_by_chunk_id(partition_chunk)
+      shard = get_shard_by_key(key, partition_server, partition_chunk)
+      if use_remote_keys:
+        shard = modify_shard(shard)
+      #print(key, shard)
+      if shard:
+        chunks = chunks_by_name(shard)
+        val = get_value(key, chunks)
+        #print(val)
+        ans += val
+        found = True
+        break
+    if not found:
+      raise "Can not find shard containing key:{}".format(key)
 
-demo()
+  print(ans)
+
+
+print(dfs.files())
+calculate_sum("data\keys", use_remote_keys=False)
