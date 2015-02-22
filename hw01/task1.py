354bdf727e98fa1f806ead54b348ece6073b4dc6
neznaika94 <andreyspbsu94@gmail.com>
2015-02-22 16:38:11 +0000

    Version 1.6: Now we find out where is /partitions and /keys chinks

--- hw01/task1.py
+++ hw01/task1.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python
 # encoding: utf8
 
-#import test_dfs as dfs
+import test_dfs as dfs
 
-import http_dfs as dfs
+#import http_dfs as dfs
 
 # Эту функцию надо реализовать. Функция принимает имя файла и
 # возвращает итератор по его строкам.
@@ -24,34 +24,51 @@ def get_server_for_chunk(chunk):
        
 # эту функцию надо реализовать. Она принимает название файла с ключами и возвращает
 # число
-def calculate_sum(keys_filename):
+def calculate_sum(key_chunk_name, chunkserver):
   result = 0  
   # При использовании http_dfs читаем с данного сервера
   #chunk_iterator = dfs.get_chunk_data("104.155.8.206", "partitions")
-  for keys in get_file_content(keys_filename):
+  for keys in dfs.get_chunk_data(chunkserver, key_chunk_name):
       key = keys[:-1]
-    #  for line in dfs.get_chunk_data("cs0", "partitions"):
-      for line in dfs.get_chunk_data("104.155.8.206", "partitions"):  
-          (begin, end, shard_name) = line[:-1].split(' ')
-          if key >= begin and key <= end:
-              chunks = get_chunks(shard_name)
-              for chunk in chunks:
-                  haveFoundKey = False #уже нашли текущий ключ
-                  server_name = get_server_for_chunk(chunk)
-                  for line in dfs.get_chunk_data(server_name, chunk):
-                      if line == '\n':
-                          break
-                      (chunk_key, chunk_value) = line[:-1].split(' ')
-                      if key == chunk_key:
-                          result += int(chunk_value)
-                          haveFoundKey = True
-                          break
-                  if haveFoundKey:
-                      break
-              break
-   
+      partitions = ""
+      for f in dfs.files():
+          if f.name == "/partitions":
+              partitions = f.chunks
+      for partition in partitions:
+          for c in dfs.chunk_locations():
+              if c.id == partition:
+                  for line in dfs.get_chunk_data(c.chunkserver, partition):
+                 # for line in dfs.get_chunk_data("104.155.8.206", "partitions"):  
+                      (begin, end, shard_name) = line[:-1].split(' ')
+                      if key >= begin and key <= end:
+                          chunks = get_chunks(shard_name)
+                          for chunk in chunks:
+                              haveFoundKey = False #уже нашли текущий ключ
+                              server_name = get_server_for_chunk(chunk)
+                              for line in dfs.get_chunk_data(server_name, chunk):
+                                  if line == '\n':
+                                      break
+                                  (chunk_key, chunk_value) = line[:-1].split(' ')
+                                  if key == chunk_key:
+                                      result += int(chunk_value)
+                                      haveFoundKey = True
+                                      break
+                              if haveFoundKey:
+                                  break
+                          break 
   return result           
   
-#demo()
-result_sum = calculate_sum('data/keys')
+#files
+#получить чанки
+#пробегаюсь и суммирую
+result_sum = 0
+keys = ""
+for f in dfs.files():
+    if f.name == "/keys":
+        keys = f.chunks
+        break
+for key in keys:    
+    for c in dfs.chunk_locations():
+        if c.id == key:
+            result_sum += calculate_sum(key, c.chunkserver)
 print(result_sum)
