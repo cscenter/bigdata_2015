66f54180bd212331420502dc172f9944a24781fc
2xmax <2xmax@mail.ru>
2015-02-22 20:22:44 +0300

    optimized version

--- hw01/task1.py
+++ hw01/task1.py
@@ -2,50 +2,64 @@
 # encoding: utf8
 
 # for local testing
-import test_dfs as dfs
+# import test_dfs as dfs
 
 # for work with real environment
-# import http_dfs as dfs
+import http_dfs as dfs
+
+from collections import defaultdict
 
 
 def get_file_content(filename):
-    chunk_ids = [f.chunks for f in dfs.files() if f.name == filename]
+    chunk_ids = [f.chunks for f in files_cache if f.name == filename]
     if not chunk_ids:
         raise Exception("file %s were not found" % filename)
-    locations = dfs.chunk_locations()
-    for chunkId in chunk_ids[0]:
-        chunk_server = [l.chunkserver for l in locations if l.id == chunkId][0]
-        for line in dfs.get_chunk_data(chunk_server, chunkId):
+    for chunk_id in chunk_ids[0]:
+        chunk_server = [l.chunkserver for l in locations_cache if l.id == chunk_id][0]
+        for line in dfs.get_chunk_data(chunk_server, chunk_id):
             if not line.isspace():
                 yield line[:-1]
 
 
 def get_filename(page_key):
-    for line in get_file_content("/partitions"):
+    for line in partitions_cache:
         lo, hi, filename = line.split(' ')
+        filename = filename.strip('/')
         if lo <= page_key <= hi:
             return filename
     raise Exception("page %s were not found" % page_key)
 
 
-def get_visit_count(page_key):
-    file = get_filename(page_key)
+def get_visit_count(file, page_keys):
+    ret = 0
     for line in get_file_content(file):
         curr_page_key, visit_count = line.split(' ')
-        if page_key == curr_page_key:
-            return int(visit_count)
-    raise Exception("page %s were not found" % page_key)
+        for page_key in page_keys:
+            if page_key == curr_page_key:
+                ret += int(visit_count)
+    return ret
 
 
 def calculate_sum(keys_filename):
-    """
-    :param keys_filename:
-    :return: visit count sum by page keys listed in keys_filename
-    """
+    task_file_to_keys = defaultdict(list)
+    keys = list(get_file_content(keys_filename))
+    for page_key in keys:
+        file = get_filename(page_key)
+        task_file_to_keys[file].append(page_key)
+
     total = 0
-    for line in get_file_content(keys_filename):
-        total += get_visit_count(line)
+    # todo: parallel
+    for file in task_file_to_keys.keys():
+        total += get_visit_count(file, task_file_to_keys[file])
+
     return total
 
 
-print(calculate_sum("/keys"))
+def demo():
+    print(calculate_sum("/keys"))
+
+
+locations_cache = list(dfs.chunk_locations())
+files_cache = list(dfs.files())
+partitions_cache = list(get_file_content("/partitions"))
+demo()
