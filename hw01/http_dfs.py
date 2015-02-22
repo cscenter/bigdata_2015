3efc3c55d4ab3fbcbfb45d829c85ed148f3efcfa
Anastasia Sulyagina <anastasia.sulyagina@gmail.com>
2015-02-23 02:35:02 +0300

    Hometask #1 v1.0

--- hw01/http_dfs.py
+++ hw01/http_dfs.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python
 # encoding: utf8
 from collections import namedtuple
-#Use this import if you're using Python3
 from urllib.request import urlopen
-#Use this import if you're using Python2
-#from urllib2 import urlopen
 import json
 
 MASTER_URL = "bigdata-hw01.barashev.net"
--- hw01/task1.py
+++ hw01/task1.py
@@ -1,16 +1,12 @@
 #!/usr/bin/python
 # encoding: utf8
+# import time
+# import test_dfs as dfs
+import http_dfs as dfs
 
-# Для быстрого локального тестирования используйте модуль test_dfs
-import test_dfs as dfs
-
-# Для настоящего тестирования используйте модуль http_dfs
-#import http_dfs as dfs
 
 # Демо показывает имеющиеся в DFS файлы, расположение их фрагментов
 # и содержимое фрагмента "partitions" с сервера "cs0"
-# (не рассчитывайте, что эти две константы останутся неизменными в http_dfs. Они
-#  использованы исключительно для демонстрации)
 def demo():
   for f in dfs.files():
     print("File {0} consists of fragments {1}".format(f.name, f.chunks))
@@ -18,31 +14,59 @@ def demo():
   for c in dfs.chunk_locations():
     print("Chunk {0} sits on chunk server {1}".format(c.id, c.chunkserver))
 
-  # Дальнейший код всего лишь тестирует получение фрагмента, предполагая, что известно,
-  # где он лежит. Не рассчитывайте, что этот фрагмент всегда будет находиться
-  # на использованных тут файл-серверах
-
-  # При использовании test_dfs читаем из каталога cs0
   chunk_iterator = dfs.get_chunk_data("cs0", "partitions")
+  # chunk_iterator = dfs.get_chunk_data("104.155.8.206", "partitions")
 
-  # При использовании http_dfs читаем с данного сервера
-  #chunk_iterator = dfs.get_chunk_data("104.155.8.206", "partitions")
   print("\nThe contents of chunk partitions:")
   for line in chunk_iterator:
-    # удаляем символ перевода строки
     print(line[:-1])
 
-# Эту функцию надо реализовать. Функция принимает имя файла и
-# возвращает итератор по его строкам.
-# Если вы не знаете ничего про итераторы или об их особенностях в Питоне,
-# погуглите "python итератор генератор". Вот например
-# http://0agr.ru/blog/2011/05/05/advanced-python-iteratory-i-generatory/
+
+# Функция ищет файл в РФС по имени
+def get_file(filename):
+    for file in dfs.files():
+        if filename == file.name:
+            return file
+    raise Exception("File %s does not exist" % filename)
+
+
+# Функция принимает имя файла и возвращает итератор по его строкам.
 def get_file_content(filename):
-  raise "Comment out this line and write your code below"
+    file = get_file(filename)
+    chunks = [chunk for chunk in dfs.chunk_locations() for chunk_id in file.chunks if chunk.id == chunk_id]
+    for chunk in chunks:
+        for line in dfs.get_chunk_data(chunk.chunkserver, chunk.id):
+            yield line
 
-# эту функцию надо реализовать. Она принимает название файла с ключами и возвращает
-# число
+
+# Функция ищет файл, содержащий ключ в диапазоне значений
+def get_key_file(key):
+    # chunk_iterator = dfs.get_chunk_data("cs0", "partitions")
+    chunk_iterator = dfs.get_chunk_data("104.155.8.206", "partitions")
+    for chunk in chunk_iterator:
+        if len(chunk) > 1:
+            (first, last, file) = chunk[:-1].split()
+            if (key >= first) and (key <= last):
+                return file
+    raise Exception("Key %s is not found" % key)
+
+
+# Функция принимает название файла с ключами и возвращает сумму их значений
 def calculate_sum(keys_filename):
-  raise "Comment out this line and write your code below"
+    keys = [key[:-1] for key in get_file_content(keys_filename) if len(key) > 1]
+    sum = 0
+    for key in keys:
+        key_file = get_key_file(key)
+        for line in get_file_content(key_file):
+            if len(line) > 1:
+                (new_key, value) = line[:-1].split()
+                if key == new_key:
+                    sum += int(value)
+                    break
+    return sum
 
-demo()
+# start = time.time()
+print(calculate_sum("/keys"))
+# finish = time.time()
+# print(finish - start)
+# demo()
