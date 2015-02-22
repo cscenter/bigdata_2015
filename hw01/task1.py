c71af6b909ae149f5bbe2a384caf18cf5962b0f1
Lioznov Anton <Lavton@gmail.com>
2015-02-23 01:41:37 +0300

    add some comments

--- hw01/task1.py
+++ hw01/task1.py
@@ -4,7 +4,6 @@
 # Для быстрого локального тестирования используйте модуль test_dfs
 # import test_dfs as dfs
 from collections import Counter, defaultdict
-import sys
 
 # Для настоящего тестирования используйте модуль http_dfs
 import http_dfs as dfs
@@ -44,9 +43,11 @@ def get_file_content(filename):
   server = locate[filename]
   return dfs.get_chunk_data(server, filename)
 
+
+# соотносит каждому ключу имя шарда, в котором он содержится 
+# и порядковый номер фрагмента этого шарда.
 def find_keys(keys, partitions):
   key_num = 0
-  part_num = 0 # хотим найти соответствия за линию. Можем это сделать, т.к. оба отсоритрованы
   res_dict = {}
   count = Counter() # будем считать, сколько каких фрагментов встретилось, чтобы потом быстро к этому фрагменту обратиться
   this_part = next(partitions)
@@ -59,23 +60,28 @@ def find_keys(keys, partitions):
       par_line = this_part.strip().split()
       continue # встречаются пустые строки
 
+    # если попали в диапазон
     if (keys[key_num] >= par_line[0]) and (keys[key_num] <= par_line[1]):
       res_dict[keys[key_num]] = (par_line[2], count[par_line[2]] - 1)
       key_num += 1
 
     if (key_num < len(keys)):
+      # читаем следующий только если для данного диапазона больше нет ключей
       if not ((keys[key_num] >= par_line[0]) and (keys[key_num] <= par_line[1])):
-        this_part = next(partitions) #читаем следующее, если тут ключи уже закончались
+        this_part = next(partitions)
         par_line = this_part.strip().split()
         count[par_line[2]] += 1
   return res_dict
 
+
+# принимает словарь, полученный в find_keys и выдаёт собственно имена фрагментов
 def get_fragment_name(key_value):
   file_list = {l.name:l.chunks for l in dfs.files()}
   keys = {key:file_list[key_value[key][0]][key_value[key][1]] for key in key_value}
   return keys
     
 
+# принимает массив из ключей, итератор содержимого файла и счётчик, сколько раз какой ключ встретился
 def get_sum_in_file(keys, file_content, keys_count):
   keys.sort()
   key_num = 0
@@ -85,6 +91,7 @@ def get_sum_in_file(keys, file_content, keys_count):
     key_line = this_key.strip().split()
     if not key_line:
       continue # встречаются пустые строки
+
     if keys[key_num] == key_line[0]:
       key_sum += int(key_line[1]) * keys_count[keys[key_num]]
       key_num += 1
@@ -97,16 +104,17 @@ def get_sum_in_file(keys, file_content, keys_count):
 def calculate_sum(keys_filename):
   keys = sorted([line.strip() for line in open(keys_filename)])
   locate = dict(dfs.chunk_locations())
+  # получим диапазоны фрагментов
   partitions = get_file_content("partitions")
-  
+  # получим названия фрагментов
   fragments = get_fragment_name(find_keys(keys, partitions))
   shards = defaultdict(list)
-  keys_count = Counter(keys)
-  for key in fragments:
+  keys_count = Counter(keys) # не будем терять повторяющиеся ключи
+  for key in fragments: # раньше каждому ключу сопостовляли фрагмент. Теперь имеющимся фрагментом сопоставим ключи, в них лежащие
     shards[fragments[key]].append(key)
   total_sum = 0
   for shard in shards:
-    total_sum += get_sum_in_file(shards[shard], get_file_content(shard), keys_count)  # мы потеряли повторяющиеся ключи. восстановим их и запишем все для файла
+    total_sum += get_sum_in_file(shards[shard], get_file_content(shard), keys_count)
   return total_sum
 
 print(calculate_sum("data/keys"))
