42a5b0598755d9bfe01011590e238f3ef3a292ba
Philipp <phil.dolgolev@gmail.com>
2015-02-22 18:29:08 +0300

    Alternative calculate_sum deleted

--- hw01/task1.py
+++ hw01/task1.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python
 # encoding: utf8
 import time
+import array
+
 # Для быстрого локального тестирования используйте модуль test_dfs
 # import test_dfs as dfs
 
@@ -49,13 +51,7 @@ def get_file_content(filename):
     return (line[:-1] for c in file_chunks for line in dfs.get_chunk_data(c.chunkserver, c.id)) # возвращаем генератор строчек файла, обрезая символ перехода строки
 
 
-#следующие 3 функции вытаскивают тройку значений из строки (границы диапозона и имя файла)
-def get_start(diaposone):
-    return diaposone.split(' ')[0]
-def get_finish(diaposone):
-    return diaposone.split(' ')[1]
-def get_filename(diaposone):
-    return diaposone.split(' ')[2][:-1]
+
 
 #функция возвращает имя файла, в котором возможно хранится данный ключ
 def get_filename_for_key(key):
@@ -63,13 +59,13 @@ def get_filename_for_key(key):
     for d in dfs.get_chunk_data("104.155.8.206", "partitions"):
     # при использовании test_dfs
     # for d in dfs.get_chunk_data("cs0", "partitions"):
+
         if (len(d[:-1]) == 0): # проверяем строку на пустую
             continue
-        start = get_start(d)
-        finish = get_finish(d)
-        filename = get_filename(d)
+        (start, finish, filename) = d[:-1].split(' ')
         if (key >= start) and (key <= finish):
             return filename[1:] # костыль для удаления символа '/', т.к. имена файлов в partition и в files различаются
+
     raise "File for key {0} not found".format(key)
 
 
@@ -86,38 +82,10 @@ def calculate_sum(keys_filename):
             if len(l) == 0: # игнорируем пустые строчки
                 continue
             if (k == l.split(' ')[0]): # сраниваем ключ с ключём в файле
-                sum += int(l.split(' ')[1]) # прибавляем из файла значение соответствующего ключу
+                sum += int(l.split(' ')[1]) # прибавляем из файла значение соответствующеe ключу
                 break
     return sum
 
-
-def get_file_chunks(filename):
-    f = find_file(filename)
-    return (c for fragment in f.chunks for c in dfs.chunk_locations() if c.id == fragment)
-
-#проверяет, может ли содержаться данный ключ в данном чанке
-def key_in_chunk(key, chunk):
-    data = dfs.get_chunk_data(chunk.chunkserver, chunk.id)
-    first = data.__next__()
-    last = first
-    for l in data:
-        last = l
-    return (key >= first) and (key <= last)
-
-def find_key_value_in_chunk(key, chunk):
-    for l in dfs.get_chunk_data(chunk.chunkserver, chunk.id):
-        if key == l.split(' ')[0]:
-            return int(l.split(' ')[1])
-
-def calculate_sum2(keys_filename):
-    sum = 0
-    keys = get_file_content(keys_filename)
-    for k in keys:
-        filename = get_filename_for_key(k)
-        for c in get_file_chunks(filename):
-            if key_in_chunk(k, c):
-                sum += find_key_value_in_chunk(k, c)
-    return sum
 
 
 # demo()
@@ -126,10 +94,5 @@ print(calculate_sum("/keys"))
 finish = time.time()
 print(finish - start)
 
-# оказалось медленнее процентов на 20
-# start = time.time()
-# print(calculate_sum2("/keys"))
-# finish = time.time()
-# print(finish - start)
 
 
