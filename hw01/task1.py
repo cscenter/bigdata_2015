4cc52d1ca2dce6411ea857b214cd23cdc1dd7ef7
Ravil <dehasi@yandex.ru>
2015-02-22 21:53:40 +0300

    Финальный (надеюсь) коммит. Добавлены эксепшены.
    Signed-off-by: Ravil <dehasi@yandex.ru>

--- hw01/task1.py
+++ hw01/task1.py
@@ -2,10 +2,10 @@
 # encoding: utf8
 
 # Для быстрого локального тестирования используйте модуль test_dfs
-# import test_dfs as dfs
+№import test_dfs as dfs
 
 # Для настоящего тестирования используйте модуль http_dfs
- import http_dfs as dfs
+import http_dfs as dfs
 
 # Демо показывает имеющиеся в DFS файлы, расположение их фрагментов
 # и содержимое фрагмента "partitions" с сервера "cs0"
@@ -36,12 +36,16 @@ def get_file_content(filename):
     if f.name == filename:
       looking_file = f
       break
+  if (looking_file == None): raise Exception("Can't find file")
+  
   for chunk in looking_file.chunks: 
     server = None
     for c in dfs.chunk_locations(): 
       if  c.id == chunk :
       	server = c
       	break
+    if (server == None): raise Exception("Can't find server")
+    
     with dfs.get_chunk_data(server.chunkserver, chunk) as part_file:
       for line in part_file:
         yield(line[:-1])       # удаляем символ перевода строки 
@@ -50,7 +54,6 @@ def get_file_content(filename):
 # эту функцию надо реализовать. Она принимает название файла с ключами и возвращает 
 # число
 # я полагаю что ключи не отсортированы и надо бы их отсортировать.
-# ток надо еще придумать как этим посползоваться
 # ну и пока надеюсь что весь файл с ключами вместится в память. 
 # 
 def calculate_sum(keys_filename = "./data/keys"): 
@@ -59,23 +62,26 @@ def calculate_sum(keys_filename = "./data/keys"):
   f.close()
   keys = sorted(list((set(s.split('\n')))))
   if len(keys[0]) < 1: keys = keys[1:]
+  
   myCoolSum = 0;
   index = 0
   keysLen = len(keys)
+  if (keysLen < 1) : raise Exception("Key is not exists")
+
   for diapazon in get_file_content("/partitions"):
-    a,b,filename = diapazon.split()
+    left, right, filename = diapazon.split()
     for i in range(index, keysLen):
-      if (a <= keys[i]) and (keys[i] <= b) :
-        for d in get_file_content(filename):
-       	  if(len(d) < 2):continue
-          k,v = d.split()
-          if (k == keys[i]):
-            myCoolSum += int(v);
+      if (left <= keys[i]) and (keys[i] <= right) :
+        for dictionary in get_file_content(filename):
+       	  if (len(dictionary) < 2) : continue #считаем что это пустая строка
+          key, value = dictionary.split()
+          if (key == keys[i]):
+            myCoolSum += int(value);
             break
       else :
         index = i
         break
-  return myCoolSum ;
+  return myCoolSum;
 
 #########           ################
 print("sum = ", calculate_sum())
