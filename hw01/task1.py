44fc53d7962c47f011fdfdc6b85550615c54a927
Philipp <phil.dolgolev@gmail.com>
2015-02-22 15:26:17 +0300

    added alternative calculate sum

--- hw01/task1.py
+++ hw01/task1.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 # encoding: utf8
-
+import time
 # Для быстрого локального тестирования используйте модуль test_dfs
 # import test_dfs as dfs
 
@@ -46,12 +46,8 @@ def find_file(filename):
 def get_file_content(filename):
     f = find_file(filename)
     file_chunks = (c for fragment in f.chunks for c in dfs.chunk_locations() if c.id == fragment) # находим все чанки этого файла
-    return (line[:-1] for c in file_chunks for line in dfs.get_chunk_data(c.chunkserver, c.id) if len(line[:-1]) > 0) # возвращаем генератор строчек файла, обрезая символ перехода строки, и игнорируя пустые строчки
+    return (line[:-1] for c in file_chunks for line in dfs.get_chunk_data(c.chunkserver, c.id)) # возвращаем генератор строчек файла, обрезая символ перехода строки
 
-#не совсем понял где будет хранится файл с ключами, эта функция считывает ключи из файла который не в РФС
-# def get_local_file_content(filename):
-#     file = open(filename)
-#     return (line[:-1] for line in file)
 
 #следующие 3 функции вытаскивают тройку значений из строки (границы диапозона и имя файла)
 def get_start(diaposone):
@@ -67,31 +63,73 @@ def get_filename_for_key(key):
     for d in dfs.get_chunk_data("104.155.8.206", "partitions"):
     # при использовании test_dfs
     # for d in dfs.get_chunk_data("cs0", "partitions"):
+        if (len(d[:-1]) == 0): # проверяем строку на пустую
+            continue
         start = get_start(d)
         finish = get_finish(d)
         filename = get_filename(d)
         if (key >= start) and (key <= finish):
-            return filename
+            return filename[1:] # костыль для удаления символа '/', т.к. имена файлов в partition и в files различаются
     raise "File for key {0} not found".format(key)
+
 
 # эту функцию надо реализовать. Она принимает название файла с ключами и возвращает
 # число
 def calculate_sum(keys_filename):
     sum = 0
-    # если файл с ключами находится в нашей РФС
     keys = get_file_content(keys_filename)
-    # keys = get_local_file_content(keys_filename)
     for k in keys:
-        print(k)
+        if len(k) == 0: #игнорируем пустые строчки
+            continue
         filename = get_filename_for_key(k)
         for l in get_file_content(filename):
+            if len(l) == 0: # игнорируем пустые строчки
+                continue
             if (k == l.split(' ')[0]): # сраниваем ключ с ключём в файле
                 sum += int(l.split(' ')[1]) # прибавляем из файла значение соответствующего ключу
                 break
     return sum
 
 
+def get_file_chunks(filename):
+    f = find_file(filename)
+    return (c for fragment in f.chunks for c in dfs.chunk_locations() if c.id == fragment)
+
+#проверяет, может ли содержаться данный ключ в данном чанке
+def key_in_chunk(key, chunk):
+    data = dfs.get_chunk_data(chunk.chunkserver, chunk.id)
+    first = data.__next__()
+    last = first
+    for l in data:
+        last = l
+    return (key >= first) and (key <= last)
+
+def find_key_value_in_chunk(key, chunk):
+    for l in dfs.get_chunk_data(chunk.chunkserver, chunk.id):
+        if key == l.split(' ')[0]:
+            return int(l.split(' ')[1])
+
+def calculate_sum2(keys_filename):
+    sum = 0
+    keys = get_file_content(keys_filename)
+    for k in keys:
+        filename = get_filename_for_key(k)
+        for c in get_file_chunks(filename):
+            if key_in_chunk(k, c):
+                sum += find_key_value_in_chunk(k, c)
+    return sum
+
+
 # demo()
-print(calculate_sum("data\keys"))
+start = time.time()
+print(calculate_sum("/keys"))
+finish = time.time()
+print(finish - start)
+
+# оказалось медленнее процентов на 20
+# start = time.time()
+# print(calculate_sum2("/keys"))
+# finish = time.time()
+# print(finish - start)
 
 
