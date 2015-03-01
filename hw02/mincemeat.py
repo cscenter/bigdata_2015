3f173c2087d587205f9ffeb641df0d298c625c42
Khazhoyan Arsen <khazhoyan.arsen@gmail.com>
2015-03-01 23:54:04 +0300

    Revert "1.0 ver. (even without output)"
    This reverts commit bdc8030e95482d2f8dc40837025deb9a4a7f064e.

--- hw02/mincemeat.py
+++ hw02/mincemeat.py
@@ -46,6 +46,7 @@ from dfs_client import *
 
 VERSION = 0.0
 
+
 DEFAULT_PORT = 11235
 
 
@@ -70,15 +71,15 @@ class Protocol(asynchat.async_chat):
         if data:
             pdata = pickle.dumps(data)
             command += str(len(pdata))
-            logging.debug("<- %s" % command)
+            logging.debug( "<- %s" % command)
             self.push(command + "\n" + pdata)
         else:
-            logging.debug("<- %s" % command)
+            logging.debug( "<- %s" % command)
             self.push(command + "\n")
 
     def found_terminator(self):
         if not self.auth == "Done":
-            command, data = (''.join(self.buffer).split(":", 1))
+            command, data = (''.join(self.buffer).split(":",1))
             self.process_unauthed_command(command, data)
         elif not self.mid_command:
             logging.debug("-> %s" % ''.join(self.buffer))
@@ -90,7 +91,7 @@ class Protocol(asynchat.async_chat):
                 self.mid_command = command
             else:
                 self.process_command(command)
-        else:  # Read the data segment from the previous command
+        else: # Read the data segment from the previous command
             if not self.auth == "Done":
                 logging.fatal("Recieved pickled data from unauthed source")
                 sys.exit(1)
@@ -122,12 +123,12 @@ class Protocol(asynchat.async_chat):
         commands = {
             'challenge': self.respond_to_challenge,
             'disconnect': lambda x, y: self.handle_close(),
-        }
+            }
 
         if command in commands:
             commands[command](command, data)
         else:
-            logging.critical("Unknown command received: %s" % (command,))
+            logging.critical("Unknown command received: %s" % (command,)) 
             self.handle_close()
 
     def process_unauthed_command(self, command, data=None):
@@ -135,20 +136,20 @@ class Protocol(asynchat.async_chat):
             'challenge': self.respond_to_challenge,
             'auth': self.verify_auth,
             'disconnect': lambda x, y: self.handle_close(),
-        }
+            }
 
         if command in commands:
             commands[command](command, data)
         else:
-            logging.critical("Unknown unauthed command received: %s" % (command,))
+            logging.critical("Unknown unauthed command received: %s" % (command,)) 
             self.handle_close()
-
+        
 
 class Client(Protocol):
     def __init__(self):
         Protocol.__init__(self)
         self.mapfn = self.reducefn = self.collectfn = None
-
+        
     def conn(self, server, port):
         self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
         self.connect((server, port))
@@ -182,10 +183,10 @@ class Client(Protocol):
         self.send_command('mapdone', (data[0], results))
 
     def call_reducefn(self, command, data):
-        logging.info("Reducing %s" % str(data[0]))
+        logging.info("Reducing %s" % str(data[0].encode('utf-8')))
         results = self.reducefn(data[0], data[1])
         self.send_command('reducedone', (data[0], results))
-
+        
     def process_command(self, command, data=None):
         commands = {
             'mapfn': self.set_mapfn,
@@ -193,7 +194,7 @@ class Client(Protocol):
             'reducefn': self.set_reducefn,
             'map': self.call_mapfn,
             'reduce': self.call_reducefn,
-        }
+            }
 
         if command in commands:
             commands[command](command, data)
@@ -224,7 +225,7 @@ class Server(asyncore.dispatcher, object):
         except:
             self.close()
             raise
-
+        
         return self.taskmanager.results
 
     def handle_accept(self):
@@ -238,7 +239,7 @@ class Server(asyncore.dispatcher, object):
     def set_map_input(self, map_input):
         self._map_input = map_input
         self.taskmanager = TaskManager(self._map_input, self)
-
+    
     def get_map_input(self):
         return self._map_input
 
@@ -277,7 +278,7 @@ class ServerChannel(Protocol):
         commands = {
             'mapdone': self.map_done,
             'reducedone': self.reduce_done,
-        }
+            }
 
         if command in commands:
             commands[command](command, data)
@@ -292,12 +293,10 @@ class ServerChannel(Protocol):
         if self.server.collectfn:
             self.send_command('collectfn', marshal.dumps(self.server.collectfn.func_code))
         self.start_new_task()
-
-
+    
 class MapInput:
     def next(self):
         pass
-
 
 class DictMapInput(MapInput):
     def __init__(self, datasource_dict):
@@ -307,7 +306,6 @@ class DictMapInput(MapInput):
     def next(self):
         key = self.dict_iter.next()
         return key, self.datasource_dict[key]
-
 
 class TaskManager:
     START = 0
@@ -324,7 +322,7 @@ class TaskManager:
         if self.state == TaskManager.START:
             self.working_maps = {}
             self.map_results = {}
-            # self.waiting_for_maps = []
+            #self.waiting_for_maps = []
             self.state = TaskManager.MAPPING
         if self.state == TaskManager.MAPPING:
             try:
@@ -352,7 +350,7 @@ class TaskManager:
         if self.state == TaskManager.FINISHED:
             self.server.handle_close()
             return ('disconnect', None)
-
+    
     def map_done(self, data):
         # Don't use the results if they've already been counted
         if not data[0] in self.working_maps:
@@ -363,7 +361,7 @@ class TaskManager:
                 self.map_results[key] = []
             self.map_results[key].extend(values)
         del self.working_maps[data[0]]
-
+                                
     def reduce_done(self, data):
         # Don't use the results if they've already been counted
         if not data[0] in self.working_reduces:
@@ -372,16 +370,15 @@ class TaskManager:
         self.results[data[0]] = data[1]
         del self.working_reduces[data[0]]
 
-
 def run_client():
-    parser = optparse.OptionParser(usage="%prog [options]", version="%%prog %s" % VERSION)
+    parser = optparse.OptionParser(usage="%prog [options]", version="%%prog %s"%VERSION)
     parser.add_option("-p", "--password", dest="password", default="", help="password")
     parser.add_option("-P", "--port", dest="port", type="int", default=DEFAULT_PORT, help="port")
     parser.add_option("-v", "--verbose", dest="verbose", action="store_true")
     parser.add_option("-V", "--loud", dest="loud", action="store_true")
 
     (options, args) = parser.parse_args()
-
+                      
     if options.verbose:
         logging.basicConfig(level=logging.INFO)
     if options.loud:
@@ -390,19 +387,16 @@ def run_client():
     client = Client()
     client.password = options.password
     client.conn(args[0], options.port)
-
+                      
 
 if __name__ == '__main__':
     run_client()
-
 
 def dump_results(results):
     keys = results.keys()
     keys.sort()
     for key in keys:
-        print
-        key + "\t" + str(results[key])
-
+        print key + "\t" + str(results[key])
 
 ##################################################################
 # Дополнительные входы для мапперов
@@ -414,19 +408,18 @@ class MapInputDFSFileLineByLine(MapInput):
 
 
     def get_generator(self, shards):
-        for shard in shards:
-            try:
-                for line in get_file_content(shard):
-                    line = line[:-1]
-                    if len(line) == 0:
-                        continue
-                    yield shard, line
-            except StopIteration:
-                pass
+      for shard in shards:
+          try:
+              for line in get_file_content(shard):
+                  line = line[:-1]
+                  if len(line) == 0:
+                      continue
+                  yield shard, line
+          except StopIteration:
+              pass
 
     def next(self):
         return next(self.generator)
-
 
 # Подает на вход мапперу пары (имя файла, имя файла)
 class MapInputDFSFileName(MapInput):
