f76f71604083d7c0e49d350b709f7ae95d5b78c6
PhilippDolgolev <phil.dolgolev@gmail.com>
2015-03-02 00:17:46 +0300

    alpha version

--- hw02/mincemeat.py
+++ hw02/mincemeat.py
@@ -37,6 +37,8 @@ import socket
 import sys
 import types
 
+import json
+
 from collections import deque
 from collections import Counter
 import codecs
@@ -128,7 +130,7 @@ class Protocol(asynchat.async_chat):
         if command in commands:
             commands[command](command, data)
         else:
-            logging.critical("Unknown command received: %s" % (command,)) 
+            logging.critical("Unknown command received: %s" % (command,))
             self.handle_close()
 
     def process_unauthed_command(self, command, data=None):
@@ -141,15 +143,15 @@ class Protocol(asynchat.async_chat):
         if command in commands:
             commands[command](command, data)
         else:
-            logging.critical("Unknown unauthed command received: %s" % (command,)) 
+            logging.critical("Unknown unauthed command received: %s" % (command,))
             self.handle_close()
-        
+
 
 class Client(Protocol):
     def __init__(self):
         Protocol.__init__(self)
         self.mapfn = self.reducefn = self.collectfn = None
-        
+
     def conn(self, server, port):
         self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
         self.connect((server, port))
@@ -186,7 +188,7 @@ class Client(Protocol):
         logging.info("Reducing %s" % str(data[0].encode('utf-8')))
         results = self.reducefn(data[0], data[1])
         self.send_command('reducedone', (data[0], results))
-        
+
     def process_command(self, command, data=None):
         commands = {
             'mapfn': self.set_mapfn,
@@ -225,7 +227,7 @@ class Server(asyncore.dispatcher, object):
         except:
             self.close()
             raise
-        
+
         return self.taskmanager.results
 
     def handle_accept(self):
@@ -239,7 +241,7 @@ class Server(asyncore.dispatcher, object):
     def set_map_input(self, map_input):
         self._map_input = map_input
         self.taskmanager = TaskManager(self._map_input, self)
-    
+
     def get_map_input(self):
         return self._map_input
 
@@ -293,7 +295,7 @@ class ServerChannel(Protocol):
         if self.server.collectfn:
             self.send_command('collectfn', marshal.dumps(self.server.collectfn.func_code))
         self.start_new_task()
-    
+
 class MapInput:
     def next(self):
         pass
@@ -350,7 +352,7 @@ class TaskManager:
         if self.state == TaskManager.FINISHED:
             self.server.handle_close()
             return ('disconnect', None)
-    
+
     def map_done(self, data):
         # Don't use the results if they've already been counted
         if not data[0] in self.working_maps:
@@ -361,7 +363,7 @@ class TaskManager:
                 self.map_results[key] = []
             self.map_results[key].extend(values)
         del self.working_maps[data[0]]
-                                
+
     def reduce_done(self, data):
         # Don't use the results if they've already been counted
         if not data[0] in self.working_reduces:
@@ -378,7 +380,7 @@ def run_client():
     parser.add_option("-V", "--loud", dest="loud", action="store_true")
 
     (options, args) = parser.parse_args()
-                      
+
     if options.verbose:
         logging.basicConfig(level=logging.INFO)
     if options.loud:
@@ -387,7 +389,7 @@ def run_client():
     client = Client()
     client.password = options.password
     client.conn(args[0], options.port)
-                      
+
 
 if __name__ == '__main__':
     run_client()
@@ -431,6 +433,19 @@ class MapInputDFSFileName(MapInput):
     def get_generator(self, files):
         for f in files:
             yield f, f
+
+    def next(self):
+        return next(self.generator)
+
+class MapInputLocalFile(MapInput):
+    generator = None
+
+    def __init__(self, file):
+        self.generator = self.get_generator(file)
+
+    def get_generator(self, file):
+        print('generator',file)
+        yield file, file
 
     def next(self):
         return next(self.generator)
