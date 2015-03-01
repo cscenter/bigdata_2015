9ae5381ffa0600498e6bd66b1015e27b43e8bd93
Mikhail Zarechenskiy <rostov93@gmail.com>
2015-03-01 15:15:39 +0300

    hw01: ready

--- hw01/task1.py
+++ hw01/task1.py
@@ -2,15 +2,15 @@
 # encoding: utf8
 
 # Для быстрого локального тестирования используйте модуль test_dfs
-import test_dfs as dfs
+# import test_dfs as dfs
 
 # Для настоящего тестирования используйте модуль http_dfs
-#import http_dfs as dfs
+import http_dfs as dfs
 
 # Демо показывает имеющиеся в DFS файлы, расположение их фрагментов
 # и содержимое фрагмента "partitions" с сервера "cs0"
 # (не рассчитывайте, что эти две константы останутся неизменными в http_dfs. Они
-#  использованы исключительно для демонстрации)
+# использованы исключительно для демонстрации)
 def demo():
   for f in dfs.files():
     print("File {0} consists of fragments {1}".format(f.name, f.chunks))
@@ -32,17 +32,79 @@ def demo():
     # удаляем символ перевода строки
     print(line[:-1])
 
+
+files = None
+locations = None
+
+def get_cached_files():
+  global files
+  if files is None:
+    files = dfs.files()
+  return files
+
+
+def get_cached_locations():
+  global locations
+  if locations is None:
+    locations = dfs.chunk_locations()
+  return locations
+
+
+def any_element(predicate, iterable):
+  for it in iterable:
+    if predicate(it):
+      return it
+
+  raise "Element not found"
+
+
 # Эту функцию надо реализовать. Функция принимает имя файла и
 # возвращает итератор по его строкам.
 # Если вы не знаете ничего про итераторы или об их особенностях в Питоне,
 # погуглите "python итератор генератор". Вот например
 # http://0agr.ru/blog/2011/05/05/advanced-python-iteratory-i-generatory/
 def get_file_content(filename):
-  raise "Comment out this line and write your code below"
+  file = any_element(lambda f: f.name == filename, get_cached_files())
+  for chunk in file.chunks:
+    location = any_element(lambda c: c.id == chunk, get_cached_locations())  # Учитываем, что сервер один
+    for line in dfs.get_chunk_data(location.chunkserver, location.id):
+      yield line
+
 
 # эту функцию надо реализовать. Она принимает название файла с ключами и возвращает
 # число
 def calculate_sum(keys_filename):
-  raise "Comment out this line and write your code below"
+  sorted_keys = sorted(map(lambda k: k[:-1], get_file_content(keys_filename)))
+  count_keys = len(sorted_keys)
 
-demo()
+  key_num = 0
+  value_sum = 0
+
+  sorted_partitions = sorted(get_file_content("/partitions"))
+  for partition in sorted_partitions:
+    if not partition.strip():
+      continue
+
+    left, right, shard_name = partition.split()
+    if left <= sorted_keys[key_num] <= right:
+      for line in get_file_content(shard_name):
+        if not line.strip():
+          continue
+
+        key, value = line.split()
+        if sorted_keys[key_num] == key:
+          value_sum += int(value)
+          key_num += 1
+        elif sorted_keys[key_num] < key:
+          break
+
+        if count_keys <= key_num:
+          break
+
+    if count_keys <= key_num:
+      break
+
+  return value_sum
+
+# demo()
+print(calculate_sum("/keys"))
