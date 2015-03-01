3a3a8a47f1b87554f720b6565fffd2ca795637d9
neznaika94 <andreyspbsu94@gmail.com>
2015-03-01 22:10:55 +0000

    mult: version 1.3: add write to chunks without write to /files yet

--- hw02/mr_mult_matrix.py
+++ hw02/mr_mult_matrix.py
@@ -1,22 +1,28 @@
 # encoding: utf-8
 from dfs_client import *
 import mincemeat
+import json
 
+# Мапперов количество файлов хранящих первую матрицу * кол-во вторую 
+# M * N различных ключей, столько и нужно мапперов
+#
+#
+#
 
 
 # маппер ожидает на входе получать ключ и значение, равные имени
 # файла, и для каждой строки (больше не помещается в память) 
 # выплевывает номер матрицы и сумму значений
+#  n = 3
+
 def mapfn(k, v):
 #  for i in range(1, 11111111):
  #     b = i
-  r = 2 #ОЛОЛОЛООЛОЛОЛОЛОЛОЛОЛОЛОЛОЛОООООООООООООООООООООООООООООООООООООООООООООООООООООООООООООООООООООООООООООО
-  n = 3
+ 
+ # value = 0    
+  r = 2
   K = 4
   m = 6
- # value = 0    
-  
-  
   row = 1  
   col = 1
   head_title = False
@@ -38,8 +44,8 @@ def mapfn(k, v):
                   continue
               if row2 >= col and row2 <= col + r - 1:
                   for cols in range(col2, col2 + r):
-                     yield '(' + str(row) + ',' + str(cols) + ')', int(l.split(" ")[(row2 - 1) % r]) * int(l2.split(" ")[(cols - 1) % r]) 
-              
+             #        print('(' + str(row) + ',' + str(cols) + ')' + ' ' +  str(int(l.split(" ")[(row2 - 1) % r]) * int(l2.split(" ")[(cols - 1) % r])) )
+                     yield '[' + str(row) + ',' + str(cols) + ']', int(l.split(" ")[(row2 - 1) % r]) * int(l2.split(" ")[(cols - 1) % r]) 
               col2 += r
               if col2 > m:
                  row2 += 1
@@ -58,6 +64,7 @@ def mapfn(k, v):
 def reducefn(k, vs):
  #   print(k)
  #   print(vs)
+ #   print(k + ' ' + str(vs))
     result = sum(vs)
     return result
     
@@ -73,5 +80,53 @@ s.mapfn = mapfn
 s.reducefn = reducefn
 
 results = s.run_server(password="") 
-for key, value in sorted(results.items()):
-    print("%s: %s" % (key, value) )
+#sum = 0
+#for key, value in sorted(results.items()):
+#    print("%s: %s" % (key, value) )
+#    sum += value
+#print(sum)        
+
+r = 2
+n = 3
+m = 6
+count_rows_in_each_matrix_dat = 2
+count_rows_in_each_chunks = 2
+#matrix_file = open('matrix.dat', 'w')
+cur_row = 1
+cur_col = 1
+matrix_dat = 'matrix_1.dat'
+matrix_dat_num = 1
+chunk_num = 1
+chunk_file = open('chunks/chunk_1_1', 'w')
+chunk_file.write(str(cur_row) + ' ' + str(cur_row + count_rows_in_each_matrix_dat - 1) + '\n')
+for key, value in sorted(results.items()):  
+    chunk_file.write(str(value))
+    if cur_col % r == 0:
+        chunk_file.write('\n')
+    else:
+        chunk_file.write(' ')
+    cur_col += 1
+    if cur_col > m:
+        cur_col = 1
+        cur_row += 1
+    if cur_row > n:    
+        break
+    if (cur_row - 1) % count_rows_in_each_matrix_dat == 0 and cur_col == 1:
+        matrix_dat_num += 1
+        matrix_dat = 'matrix_%d.dat' % matrix_dat_num
+        chunk_num = 1 
+    if (cur_row - 1) % count_rows_in_each_chunks == 0 and cur_col == 1:
+        if (cur_row - 1) % count_rows_in_each_matrix_dat != 0:
+            chunk_num += 1        
+        chunk_file = open('chunks/chunk_%d_%d' % (matrix_dat_num, chunk_num), 'w')
+        if chunk_num == 1:        
+            chunk_file.write(str(cur_row) + ' ' + str(min(n, cur_row + count_rows_in_each_matrix_dat - 1)) + '\n')
+    print(str(cur_row) + ' ' + str(cur_col) + str(matrix_dat) + ' ' + str('chunk_%d_%d' % (matrix_dat_num, chunk_num)))    
+
+
+
+
+
+
+
+
