bdc8030e95482d2f8dc40837025deb9a4a7f064e
Khazhoyan Arsen <khazhoyan.arsen@gmail.com>
2015-03-01 22:03:14 +0300

    1.0 ver. (even without output)

--- hw02/mr_sum_matrix.py
+++ hw02/mr_sum_matrix.py
@@ -6,32 +6,35 @@ import mincemeat
 # файла, и для каждой строки (больше не помещается в память) 
 # выплевывает номер матрицы и сумму значений
 def mapfn(k, v):
-  reduce_key = None
-  for l in get_file_content(v):
-    if reduce_key is None:
-      matrix_num, start, end = l.split(" ", 2)
-      reduce_key = matrix_num
-      continue
-    values = [int(v) for v in l.split(" ")]
-    yield reduce_key, sum(values)
+    reduce_key = None
+    for l in get_file_content(v):
+        if reduce_key is None:
+            matrix_num, start, end = l.split(" ", 2)
+            reduce_key = matrix_num
+            continue
+        values = [int(v) for v in l.split(" ")]
+        yield reduce_key, sum(values)
+
 
 # редьюсер суммирует значения с одинаковым ключом
 def reducefn(k, vs):
     result = sum(vs)
     return result
 
-s = mincemeat.Server() 
+
+s = mincemeat.Server()
 
 # читаем список файлов, из которых состоят матрицы
 matrix_files = [l for l in get_file_content("/matrix1")]
 for l in get_file_content("/matrix2"):
     matrix_files.append(l)
-    
+
+
 # и подаем этот список на вход мапперам
-s.map_input = mincemeat.MapInputDFSFileName(matrix_files) 
+s.map_input = mincemeat.MapInputDFSFileName(matrix_files)
 s.mapfn = mapfn
 s.reducefn = reducefn
 
-results = s.run_server(password="") 
+results = s.run_server(password="")
 for key, value in sorted(results.items()):
-    print("%s: %s" % (key, value) )
+    print("%s: %s" % (key, value))
--- hw02/print_matrix.py
+++ hw02/print_matrix.py
@@ -6,35 +6,37 @@ from dfs_client import *
 
 
 def print_matrix_chunk(filename, cols):
-  row = 0
-  col = 0
-  for l in get_file_content(filename):
-    if row == 0:
-      matrix_num, start, end = l.split(" ", 2)
-      print("Matrix #%s Rows [%s, %s]" % (matrix_num, start, end))
-      row += 1
-      continue
-    
-    print(l, end=' ')
-    col += len(l.split(" "))
-    if col == cols:
-      print("")
-      row += 1
-      col = 0
-  return row - 1
+    row = 0
+    col = 0
+    for l in get_file_content(filename):
+        if row == 0:
+            matrix_num, start, end = l.split(" ", 2)
+            print("Matrix #%s Rows [%s, %s]" % (matrix_num, start, end))
+            row += 1
+            continue
+
+        print(l, end=' ')
+        col += len(l.split(" "))
+        if col == cols:
+            print("")
+            row += 1
+            col = 0
+    return row - 1
+
 
 def print_matrix(matrix_toc, rows, cols):
-  print("Printing matrix %dx%d" % (rows, cols))
-  read_rows = 0
-  for l in get_file_content(matrix_toc):
-    read_rows += print_matrix_chunk(l, cols)
-  if rows != read_rows:
-    print("Что-то пошло не так: мы прочитали %d строк а надо было %d" % (read_rows, rows))
+    print("Printing matrix %dx%d" % (rows, cols))
+    read_rows = 0
+    for l in get_file_content(matrix_toc):
+        read_rows += print_matrix_chunk(l, cols)
+    if rows != read_rows:
+        print("Что-то пошло не так: мы прочитали %d строк а надо было %d" % (read_rows, rows))
+
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--num", required = True, help = "Номер матрицы")
-parser.add_argument("--rows", required = True, help = "Число строк")
-parser.add_argument("--cols", required = True, help = "Число столбцов")
+parser.add_argument("--num", required=True, help="Номер матрицы")
+parser.add_argument("--rows", required=True, help="Число строк")
+parser.add_argument("--cols", required=True, help="Число столбцов")
 
 args = parser.parse_args()
 print_matrix("/matrix%s" % args.num, int(args.rows), int(args.cols))
