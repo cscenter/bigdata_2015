9f5e9c97a5e87d615f9e65087f8bb123644a93e3
Alexander <alex27594@gmail.com>
2015-03-02 02:45:02 +0300

    final version

--- hw02/multiplication.py
+++ hw02/multiplication.py
@@ -2,7 +2,6 @@
 
 from dfs_client import *
 import mincemeat
-import os
 #Количество мапперов, которые нам понадобятся равно количеству различных пар файлов, первый из которых
 #хранит данные первой матрицы, а второй хранит данные второй матрицы.
 #Количество редьюсеров равно количество элементов в результирующей матрице, то есть n*m. Нужно не более n*m машин, иначе они будут простаивать
@@ -15,7 +14,10 @@ def mapfn(v1, v2):
     m = 6
     ind_1 = False
     num_of_col_1 = 1
+    num_of_row_1 = 1
+    #читаем блоки длины r из чанков файла, содержащего информацию о первой матрице
     for l1 in get_file_content(v1):
+        #проверка перехода в новый чанк
         if ind_1 == False:
             matrix_num1, start1, end1 = l1.split(" ", 2)
             num_of_row_1 = int(start1)
@@ -24,16 +26,21 @@ def mapfn(v1, v2):
         num_of_col_2 = 1
         num_of_row_2 = 1
         ind_2 = False
+        #читаем блоки длины r из чанков файла,содержащего информацию о второй матрице
         for l2 in get_file_content(v2):
             if ind_2 == False:
                 matrix_num2, start2, end2 = l2.split(" ", 2)
                 num_of_row_2 = int(start2)
                 ind_2 = True
                 continue
+            #проходимся по столбцам второй матрицы
             for col_2 in range(num_of_col_2, num_of_col_2 + r):
                 for i in range(0, r):
                     if num_of_row_2 == num_of_col_1 + i:
+                        #создаём ключ, содержащий в себе координаты элемента в результирующей матрице
                         key = '[' + str(num_of_row_1) + ',' + str(col_2) + ']'
+                        #создаём значение, равное произведению двух чисел, это произведение будет являться слагаемым в суммировании
+                        #элементов для вычисления элемента результирующей матрицы
                         value = int(l1.split(" ")[(num_of_row_2 - 1) % r]) * int(l2.split(" ")[(col_2 - 1) % r])
                         yield key, value
             num_of_col_2 += r
