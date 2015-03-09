c7bdb0d04d6da3356110743e859a2ac31a57c5a4
Alex Mylnikov <alex@mylnikov.org>
2015-03-09 16:19:36 +0300

    third2

--- hw03/my.py
+++ hw03/my.py
@@ -7,6 +7,7 @@ import sys
 import json
 import util
 from math import log
+
 sys.path.append("../dfs/")
 
 import client as dfs
@@ -20,33 +21,45 @@ metadata = dfs.CachedMetadata()
 for one in metadata.get_file_content("/wikipedia/__toc__"):
     print(one)
 
-query_in = raw_input("some query: ").strip()
-# query_in="architecture HANA"
+# query_in = raw_input("some query: ").strip()
+query_in="architecture HANA"
 D_long = int(len([document for document in dfs.get_file_content("/wikipedia/__toc__")]))
 Q_long = int(len(query_in))
+
+
+def get_tf(url, word):
+    count = 0
+    word_count = 0
+    for line in dfs.get_file_content(url):
+        # print line
+        for one_word in line.split():
+            one_word=one_word.strip()
+            count += 1
+            # print one_word
+            if word in one_word:
+                word_count += 1
+
+    return float(word_count)/float(count)
+
 
 mass = defaultdict(lambda: 0)
 
 for word in query_in.split():
     print word
 
-
     shard = "".join(
         [l for l in metadata.get_file_content("/{}/posting_list/{}".format(USERNAME, util.encode_term(word)[0:1]))])
     plists = json.JSONDecoder().decode(shard)
-    print(plists)
+    # print(plists)
     d = len(plists[util.encode_term(word)])
     idf = log(float(D_long) / float(d), 4)
     for docum in plists[util.encode_term(word)]:
         print(docum)
         D_id = docum.split("/")[2]
-
-        tf = float(1/float(d))
+        # print(get_tf(docum, word))
+        tf = get_tf(docum, word)#float(1 / float(d))
         mass[D_id] += float(tf) * idf
-    print(list(map(lambda x: x.split(), plists[util.encode_term(word)])))
-
-
-
+    # print(list(map(lambda x: x.split(), plists[util.encode_term(word)])))
 
 for D_id in mass:
     mass[D_id] /= Q_long * D_long
--- hw03/print-posting-list.py
+++ hw03/print-posting-list.py
@@ -11,10 +11,11 @@ metadata = dfs.CachedMetadata()
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--term", required = True)
-args = parser.parse_args()
-
+# args = parser.parse_args()
+# args.term = "HANA"
+ff="HANA"
 # Ваш псевдоним в виде строковой константы
 # USERNAME=
-shard = "".join([l for l in metadata.get_file_content("/%s/posting_list/%s" % (USERNAME, util.encode_term(args.term)[0:1]))])
+shard = "".join([l for l in metadata.get_file_content("/%s/posting_list/%s" % (USERNAME, util.encode_term(ff)[0:1]))])
 plists = json.JSONDecoder().decode(shard)
-print plists[util.encode_term(args.term)]
+print plists[util.encode_term(ff)]
