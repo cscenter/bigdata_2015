60772cc4623a3aedac8ec79656713904d8e38f11
izhleba <izhleba@gmail.com>
2015-03-09 14:16:19 +0300

    ДОбавил коментарии

--- hw03/README.md
+++ hw03/README.md
@@ -1,5 +1,10 @@
+Скрипт подсчета IFIDF:
+```
+python calc_tf_idf.py
+// Затем два раза
+python mincemeat.py localhost
+```
 Запуск векторного поиска:
 ```
 python full_slow_search.py -q "fast big data"
 ```
-Скрипт подсчета IFIDF: `calc_tf_idf.py`
--- hw03/full_slow_search.py
+++ hw03/full_slow_search.py
@@ -9,6 +9,10 @@ import json
 import util
 import math
 
+# Простой векторный поиск
+# Для слов из запроса определятся множестов документов где они встречается. Для каждого документа из множества,
+# определяем косинусную меру сходства вектора слов его характеризующую с вектором слов запроса. Выбираем 10 близких.
+
 metadata = dfs.CachedMetadata()
 
 parser = argparse.ArgumentParser()
@@ -18,22 +22,26 @@ args = parser.parse_args()
 query_sqtr = args.q.strip()
 word_vec = {}
 if query_sqtr:
+    # Почистим запрос от повторяющихся слов
     for word in query_sqtr.split(" "):
         word_vec[word] = True
 
 if len(word_vec.keys()) > 0:
     USERNAME = "izhleba"
-    # map of lists
+    # Подготовка списка документов для поиска
     search_area = {}
     for word in word_vec.keys():
         word_id = util.encode_term(word)
         filename = "/%s/posting_list/%s" % (USERNAME, word_id[0:1])
         shard = "".join([l for l in metadata.get_file_content(filename)])
+        # Группируем слова по документам где они встречаются
         for doc_id in json.JSONDecoder().decode(shard)[word_id]:
             if doc_id in search_area:
                 search_area[doc_id].append(word_id)
             else:
                 search_area[doc_id] = [word_id]
+
+    # Считаем косинусное сходство для всего списка документов
     search_result = []
     for doc_id_location, word_ids in search_area.iteritems():
         word_map = dict((l, 0) for l in word_ids)
@@ -44,7 +52,7 @@ if len(word_vec.keys()) > 0:
                 word_map[word_id] = TFIDF
             if word_id > max_word:
                 break
-
+        # Подсчет формулы косинусного сходства
         factor_sum = 0
         square_sum = 0
         for word_id, factor in word_map.iteritems():
@@ -53,13 +61,14 @@ if len(word_vec.keys()) > 0:
         cos_factor = factor_sum / (math.sqrt(square_sum) + math.sqrt(len(word_ids)))
         search_result.append((cos_factor, doc_id_location))
 
+    # Подготовим название статей для удобства визуализации
     wikipedia_files = {}
     for line in dfs.get_file_content("/wikipedia/__toc__"):
         pair = line.split(" ", 1)
         wikipedia_files[pair[0]] = pair[1]
 
     for factor, doc_id_location in sorted(search_result, reverse=True)[:10]:
-        print(doc_id_location, factor,wikipedia_files[doc_id_location])
+        print(doc_id_location, factor, wikipedia_files[doc_id_location])
 
 else:
     print "Query is empty"
