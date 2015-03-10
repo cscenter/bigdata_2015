f9db609ca5fe3240699f94a57bcee76bf410627c
neznaika94 <andreyspbsu94@gmail.com>
2015-03-10 02:11:17 +0000

    fix some small problems

--- hw03/mr_posting_lists.py
+++ hw03/mr_posting_lists.py
@@ -7,20 +7,10 @@ sys.path.append("../dfs/")
 
 import client as dfs
 
-# Это последовательность из двух Map-Reduce
-# Диспетчер запускается командой python mr_posting_lists.py
-# Рабочий процесс запускается командой python mincemeat.py localhost 
-# для каждого из Map-Reduce. То есть, когда отработает первый рабочий процесс, 
-# нужно запустить эту же команду еще раз
-# 
-# Этот конвейер пока что работает только на одной машине 
-# (потому что результаты первого MR записываются в локальные файлы)
-
-# Первый Map-Reduce отображает терм в документ
+# Первый Map-Reduce отображает терм в документ + tf для слова конкретно в этом документе
 def mapfn(k, v):
 	import util
 	filename, pagetitle = v.split(" ", 1)
-#	print v
 
 	import sys
 	sys.path.append("../dfs/")
@@ -39,21 +29,16 @@ def mapfn(k, v):
 	if count == 0:
 		raise Exception("File %s is empty" % filename)                
 	for word in words:
-		yield util.encode_term(word), filename + " " + str(float(words[word]) / count) + " " + str(count)
+		yield util.encode_term(word), filename + " " + str(float(words[word]) / count)
 
 # и записывает список документов для каждого терма во временный файл
 def reducefn(k, vs):
 	import util  
-#	if util.decode_term(k)[0:1] == 'h':
-#		print(util.decode_term(k))     
-#	idf = len(vs)        
 
-#	print vs 
 	if len(k) > 100:
 		print "Skipping posting list for term %s" % (util.decode_term(k))
 		return {}
-	with open("tmp/plist/%s" % k, "w") as plist:
-#		plist.write(str(idf) + "\n")             
+	with open("tmp/plist/%s" % k, "w") as plist:      
 		plist.write("\n".join(vs) + "\n")
 	return {}
 
@@ -76,13 +61,8 @@ def mapfn1(k, v):
 # свертка собирает все списки вхождений для термов, начинающихся на одну и ту же букву, 
 # составляет из них словарь, сериализует его и записывает в файл на DFS
 def reducefn1(k, vs):
-    	import util      
-     
 	term_plist = {}
 	for term in vs:
- #        	if util.decode_term(term)[0:1] == 'h': 
- #			print(term)    		
-        
 		with open("tmp/plist/%s" % term) as f:     
 			term_plist[term] = f.read().split("\n")
 
@@ -92,7 +72,6 @@ def reducefn1(k, vs):
 	import client as dfs
 	import json
  
-#	print(k)
 	# Ваш псевдоним в виде строковой константы
 	USERNAME = "andreybalakshiy"
 	with dfs.file_appender("/%s/posting_list/%s" % (USERNAME, k)) as buf:
--- hw03/search.py
+++ hw03/search.py
@@ -6,10 +6,12 @@ sys.path.append("../dfs/")
 
 import client as dfs
 
-#query = "support vector machine"
-query = "alert answers computers Gain insight"
+#query = "alert answers computers Gain insight"
+query = str(raw_input("query:\n"))
 
-wikipedia_files = [l for l in dfs.get_file_content("/wikipedia/__toc__")]
+metadata = dfs.CachedMetadata()
+
+wikipedia_files = [l for l in metadata.get_file_content("/wikipedia/__toc__")]
 D = len(wikipedia_files)
 
 import util
@@ -17,23 +19,21 @@ score = {}
 USERNAME = "andreybalakshiy"
 for q in query.split(" "):
     base = util.encode_term(q.lower())
-    f = "".join([d for d in dfs.get_file_content("/%s/posting_list/%s" % (USERNAME, base[0:1]))])
-    f_list = json.JSONDecoder().decode(f)
+    shard = "".join([d for d in metadata.get_file_content("/%s/posting_list/%s" % (USERNAME, base[0:1]))])
+    f_list = json.JSONDecoder().decode(shard)
     d_cnt = len(f_list[base])
     for l in f_list[base]:
         if l == "":
             continue
-        doc_name, tf, count = l.split(" ", 2)
+        doc_name, tf = l.split(" ", 1)
         tf = float(tf)
-        count = float(count)
         import math
         idf = math.log(float(D) / d_cnt) 
         tf_idf = tf * idf
-        tf_word_query = 1.0 / (count + 1)
-        idf_word_query = math.log(float(D + 1) / (d_cnt + 1))
-        tf_idf_word_query = tf_word_query * idf_word_query
+     #   tf_word_query = 1.0 / (count + 1)
+    #    idf_word_query = math.log(float(D + 1) / (d_cnt + 1))
+     #   tf_idf_word_query = tf_word_query * idf_word_query
 
-        # нормированное скалярное произведение
         rev = tf * idf
         if score.has_key(doc_name):
             score[doc_name] += rev
@@ -41,14 +41,13 @@ for q in query.split(" "):
             score[doc_name] = rev
             
 mx = 0.0
-n = "a";
+n = "";
 for s in score.keys():
  #   print(score[s])
     if score[s] > mx:
         mx = score[s];
         n = s            
          
-
 for l1 in dfs.get_file_content(n):
 	print l1
 print(n)  
