b800dd220de144be03472450d4ceadaf3f9c135f
Dmitry Barashev <dbarashev@bardsoftware.com>
2015-03-12 14:20:39 +0300

    pagerank mapreduce fixes

--- class04/mr_pagerank.py
+++ class04/mr_pagerank.py
@@ -11,19 +11,19 @@ def mapfn(k, v):
   import sys
   sys.path.append("../dfs/")
   import client as dfs
-  N = 10000
+  N = 1000
 
   for l in dfs.get_file_content(v):
     l = l.strip()
     if len(l) == 0:
       continue
-    cols = l.split(" ", 2)
+    cols = l.split(" ")
     if len(cols) != 2 and len(cols) != 4:
-      sys.stderr.write("Malformed record: " + l)
+      sys.stderr.write("Malformed record len=%d: %s" %(len(cols), str(l)))
       continue
 
     docid = cols[0]
-    outlinks = [] if cols == "==" else cols[1].split(",")
+    outlinks = [] if cols[1] == "==" else cols[1].split(",")
     if len(cols) == 4:
       rank = float(cols[2])
       iter_num = int(cols[3])
@@ -45,8 +45,7 @@ def reducefn(k, vs):
   import sys
   sys.path.append("../dfs/")
   import client as dfs
-  N = 10000
-  HOSTNAME = socket.gethostname()
+  N = 1000
 
   new_rank = 0.0
   iter_num = None
@@ -61,17 +60,17 @@ def reducefn(k, vs):
       sys.stderr.write("Malformed reduce task: key=%s value=%s" % (k, str(vs)))
 
   if iter_num is None:
-    sys.stderr.write("Malformed reduce tas (no iter num): key=%s value=%s" % (k, str(vs)))
+    sys.stderr.write("Malformed reduce task (no iter num): key=%s value=%s" % (k, str(vs)))
+    return
   new_rank = 0.85 * new_rank + 0.15 / N
-  out_filename = "/class04/iter%d/%s" % (iter_num, HOSTNAME)
-  with dfs.file_appender(out_filename) as buf:
-    buf.write("%s %s %f %d" % (k, outlinks, new_runk, iter_num))
-    print "%s: %s" % (out_filename, "%s %s %.15f %d" % (k, outlinks, new_rank, iter_num))
+  out_filename = "/class04/iter%d/%s" % (iter_num, WORKER_NAME)
+  print "%s %s %f %d" % (k, outlinks, new_rank, iter_num)
 
   return out_filename
 
 s = mincemeat.Server() 
 
+import argparse
 # читаем список файлов, из которых состоят матрицы
 parser = argparse.ArgumentParser()
 parser.add_argument("--toc", required = True)
