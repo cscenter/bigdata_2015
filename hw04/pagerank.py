df0d73ff3bc18afc3733cd426504668c80085d9a
Dmitry Barashev <dbarashev@bardsoftware.com>
2015-03-12 15:59:03 +0300

    some comments

--- hw04/pagerank.py
+++ hw04/pagerank.py
@@ -1,3 +1,4 @@
+# encoding: utf-8
 """pagerank.py illustrates how to use the pregel.py library, and tests
 that the library works.
 
@@ -8,16 +9,21 @@ from hw04util import *
 import sys
 
 vertices = {}
-num_workers = 2
+num_workers = 1
 
 
 def main(filename):
   global vertices
   global num_vertices
+  # читаем граф из файла, используя конструкток PageRankVertex
   vertices = read_graph(filename, PageRankVertex)
+
+  # Инициализируем начальное распределение pagerank
   num_vertices = len(vertices)
   for v in vertices.values():
     v.value = 1.0/num_vertices
+
+  # Запускаем подсчет
   pagerank_pregel(vertices.values())
 
 def pagerank_pregel(vertices):
@@ -35,15 +41,20 @@ class PageRankVertex(Vertex):
 
     def update(self):
         global vertices
+        # На 50-й итерации остановимся
         if self.superstep < 50:
+            # На нулевой итерации у нас еще нет никаких входящих сообщений, поэтому мы не меняем PR вершины
             if self.superstep > 0:
+              # Вероятность перехода с любой страницы + вероятность перехода со ссылающихся страниц
               self.value = 0.15 / num_vertices + 0.85*sum(
                   [pagerank for (vertex,pagerank) in self.incoming_messages])
             if len(self.out_vertices) == 0:
+              # тупиковая страница рассылает часть своего PR всем
               outgoing_pagerank = self.value / num_vertices
               self.outgoing_messages = [(vertex,outgoing_pagerank) 
                                         for vertex in vertices.values()]              
             else:
+              # нетупиковая только тем, на кого ссылается
               outgoing_pagerank = self.value / len(self.out_vertices)
               self.outgoing_messages = [(vertex,outgoing_pagerank) 
                                         for vertex in self.out_vertices]
