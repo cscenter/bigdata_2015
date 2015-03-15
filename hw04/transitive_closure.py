0b5599ce88d08991d613696ea3ce7884408e068c
Khazhoyan Arsen <khazhoyan.arsen@gmail.com>
2015-03-16 00:12:24 +0300

    v1.0

---
+++ hw04/transitive_closure.py
@@ -0,0 +1,49 @@
+# encoding: utf-8
+
+from pregel import Vertex, Pregel
+from hw04util import *
+import sys
+
+vertices = {}
+num_workers = 1
+max_supersteps = 50
+
+
+def main(filename):
+    global vertices
+    vertices = read_graph(filename, TransitiveClosureVertex)
+
+    p = Pregel(vertices.values(), num_workers, max_supersteps)
+    p.run()
+    print "Completed in %d supersteps" % p.superstep
+    for vertex in p.vertices:
+        print "#%s: %s" % (vertex.id, ' '.join([str(v.id) for v in vertex.out_vertices]))
+
+
+class TransitiveClosureVertex(Vertex):
+    def __init__(self, id):
+        Vertex.__init__(self, id, None, [])
+        self.new_out_vertices = []
+
+    def update(self):
+        # Considering vertex u, let's call vertex v a 'distant_vertex' if it connected to u by two consecutive edges
+
+        # Superstep consists of two step
+        # In a first step vertex sends itself to all of its distant vertices
+        if self.superstep % 2 == 0:
+            self.active = False
+            if self.new_out_vertices:
+                self.active = True
+                self.outgoing_messages = [(distant_vertex, self)
+                                          for vertex in self.new_out_vertices
+                                          for distant_vertex in vertex.new_out_vertices]
+                self.out_vertices.extend(self.new_out_vertices)
+                self.new_out_vertices[:] = []
+        # In a second step vertex get edges to all of its distant vertices
+        else:
+            for _, new_distant_vertex in self.incoming_messages:
+                new_distant_vertex.new_out_vertices.append(self)
+
+
+if __name__ == "__main__":
+    main(sys.argv[1])
