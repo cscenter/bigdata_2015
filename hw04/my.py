2af816ed73406959be8f9c7fb939a6f141f5c8dc
Dmitry Barashev <dbarashev@bardsoftware.com>
2015-03-26 20:47:18 +0300

    useless code

--- hw04/my.py
+++ hw04/my.py
@@ -14,30 +14,8 @@ def main(filename):
 
     vertices = read_graph(filename, Mylnikov)
 
-    print(vertices)
-
-    for v in vertices.values():
-        v.value = randint(1, len(vertices) * 1000)
-        print(v.value)
 
     p = Pregel(vertices.values(), num_workers, num_iterations)
-    p.run()
-    print("Done in {} supersteps".format(p.superstep))
-
-
-    for vertex in p.vertices:
-        print("#%s: %s" % (vertex.id, vertex.value))
-    print("Sum=%f" % sum(v.value for v in p.vertices))
-    for vertex in p.vertices:
-        out_vertices = ",".join(str(vertex.id) for vertex in vertex.out_vertices)
-        if not out_vertices:
-            out_vertices = "=="
-        print ("{}: {} {}".format(vertex.id, out_vertices, vertex.value))
-
-def pagerank_pregel(vertices):
-    """Computes the pagerank vector associated to vertices, using
-    Pregel."""
-    p = Pregel(vertices, num_workers, num_iterations)
     p.run()
     for vertex in p.vertices:
         out_vertices = ",".join(str(vertex.id) for vertex in vertex.out_vertices)
@@ -78,31 +56,7 @@ class Mylnikov(Vertex):
                                               for linked in
                                               n_vertexes]
 
-
-
-class PageRankVertex(Vertex):
-    def __init__(self, id):
-        Vertex.__init__(self, id, None, [])
-
-    def update(self):
-        global vertices
-
-        if self.superstep > 0:
-                   self.value = 0.15 / num_vertices + 0.85 * sum(
-                [pagerank for (vertex, pagerank) in self.incoming_messages])
-        if len(self.out_vertices) == 0:
-
-            outgoing_pagerank = self.value / num_vertices
-            self.outgoing_messages = [(vertex, outgoing_pagerank)
-                                      for vertex in vertices.values()]
-        else:
-
-            outgoing_pagerank = self.value / len(self.out_vertices)
-            self.outgoing_messages = [(vertex, outgoing_pagerank)
-                                      for vertex in self.out_vertices]
-
-
 if __name__ == "__main__":
     # main("small_graph.txt")
-    main("random_1000.txt")
-    # main(sys.argv[1])
+    #main("random_1000.txt")
+    main(sys.argv[1])
